12e2a8eabe000ee64ee9363ed339551d85b133ae
Julian Ailan <jtulians@gmail.com>
2017-03-24 15:56:39 -0300

    Removed unused code and imports.

--- cypy/credentials.py
+++ cypy/credentials.py
@@ -9,31 +9,17 @@ NUMERIC = [int, float]
 
 class Credential(object):
     """User information of assumed competence."""
-    def __init__(self, username, password, keyword=None,
-                 cyclic=False, period=1, keyword_length=10):
-        if ((type(username) and type(password) is str) and
-           (type(keyword_length) is int) and (type(period) in NUMERIC)):
-            self.cyclic = cyclic
-            if self.cyclic and not keyword:
-                self.keyword = cypher.generate_random_phrase(keyword_length)
-                self.th = cypher.IntervalThread(period, cypher.encode,
-                                                self.keyword, username,
-                                                password)
-                self._start_cyclic_encryption()
-            elif keyword:
+    def __init__(self, username, password, keyword):
+        if (type(username) and type(password) and type(keyword)) is str:
                 self.keyword = keyword
                 self.username = cypher.encode(self.keyword, username)
                 self.password = cypher.encode(self.keyword, password)
-            else:
-                raise ValueError("Incorrect set of parameters given.")
         else:
             raise ValueError("Check the types of the given parameters.")
 
     @property
     def credentials(self):
         """Returns credentials with their current encryption."""
-        if self.cyclic:
-            return self.th.username, self.th.password
         return self.username, self.password
 
     @property
@@ -41,11 +27,3 @@ class Credential(object):
         """Returns decoded credentials."""
         return (cypher.decode(self.keyword, self.username),
                 cypher.decode(self.keyword, self.password))
-
-    def _start_cyclic_encryption(self):
-        """Start cyclic credentials encryption."""
-        self.th.start()
-
-    def _stop_cyclic_encryption(self):
-        """Stop credentials encryption."""
-        return self.th.join()
--- cypy/cypher.py
+++ cypy/cypher.py
@@ -2,10 +2,7 @@
 # ===================================
 
 # Built-in modules
-import time
 import string
-import random
-from threading import Thread, Event
 from itertools import count, cycle
 
 char_to_int = string.printable[:95]
@@ -29,50 +26,3 @@ def encode(key, text):
 
 def decode(key, text):
     return vigenere(key, text, True)
-
-
-class TaskThread(Thread):
-    def __init__(self, task, key, username, password):
-        Thread.__init__(self)
-        self.task = encode if task == "encode" else decode
-        self.key = key
-        self.username = username
-        self.password = password
-        self.stopped = Event()
-        self.return_value = None
-
-    def run(self):
-        self.return_value = (self.task(self.key, self.username),
-                             self.task(self.key, self.password))
-        return self.return_value
-
-    def join(self):
-        if self.is_alive():
-            self.stopped.set()
-            Thread.join(self)
-        return self.username, self.password
-
-
-class IntervalThread(TaskThread):
-    def __init__(self, period, task, key, username, password):
-        super().__init__(task, key, username, password)
-        self.period = period
-
-    def run(self):
-        self.username, self.password = super().run()
-        while not self.stopped.is_set():
-            time.sleep(self.period)
-            self.username, self.password = super().run()
-
-
-def generate_random_phrase(length, char_set=(string.ascii_letters +
-                                             string.digits +
-                                             string.punctuation)):
-    if type(char_set) is str:
-        try:
-            return "".join(random.choice(char_set) for _ in xrange(length))
-        except NameError:
-            return "".join(random.choice(char_set) for _ in range(length))
-    else:
-        raise NameError("Make sure to provide a character set consisting of"
-                        " a string of chars. type(char_set) != str.")
